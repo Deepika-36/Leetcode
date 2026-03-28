b6a66e183e56a852a9c26c9bc8c7f226a8f788b5
Deepika B <deepuvarma20055@gmail.com>
2026-03-28 12:35:40 +0530

    Time: 3 ms (99.85%) | Memory: 94.3 MB (74.52%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,8 +10,11 @@
  */
  class Solution {
     public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null){
+            return true;
+        }
 
-        
+
         ListNode slow = head;
         ListNode fast = head;
 
