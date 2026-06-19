0a601b626939a83b03b0953e7564af40b5297eec
Deepika B <deepuvarma20055@gmail.com>
2026-06-19 09:59:30 +0530

    Time: 8 ms (43.26%) | Memory: 47 MB (57.58%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,13 +1,33 @@
+import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
-                }
+
+        int n = nums.length;
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i]; // value
+            arr[i][1] = i;       // original index
+        }
+
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            int sum = arr[left][0] + arr[right][0];
+
+            if (sum == target) {
+                return new int[]{arr[left][1], arr[right][1]};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
-        // If no valid pair is found, return an empty array instead of null
-        return new int[] {};
+
+        return new int[]{-1, -1};
     }
 }
